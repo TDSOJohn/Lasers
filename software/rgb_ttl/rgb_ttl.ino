7f2dfbfe7714da1c67190ff06d84a41e3642164d
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-05-14 18:54:32 +0200

    improved control code

--- software/rgb_ttl/rgb_ttl.ino
+++ software/rgb_ttl/rgb_ttl.ino
@@ -1,26 +1,24 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
-int timer = 10000;
+int timer = 1500;
 int counter = 0;
 
 // the loop function runs over and over again forever
 void loop() {
-  counter++;
-  if(counter > 3) {
-    timer = timer - 100;
-    if(timer <= 0)
-      timer = 10000;
-    counter = 0;
+  timer = timer - 10;
+  if(timer <= 0) {
+    timer = 1500;
+    counter = ((counter + 1) % 3);
   }
-  digitalWrite(5, HIGH);  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(4, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delayMicroseconds(timer);                      // wait for a second
-  digitalWrite(5, LOW);   // turn the LED off by making the voltage LOW
-  digitalWrite(4, LOW);   // turn the LED off by making the voltage LOW
-  delayMicroseconds(10000 - timer);                      // wait for a second
+  
+  digitalWrite(counter + 3, HIGH);   // turn the LED off by making the voltage LOW
+  delayMicroseconds(timer);   // wait for timer microseconds
+  digitalWrite(counter + 3, LOW);  // turn the LED on (HIGH is the voltage level)
+  delayMicroseconds(1500 - timer);   // wait for timer microseconds
 }
