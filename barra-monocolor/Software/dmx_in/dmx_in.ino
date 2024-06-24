04e81ada04f71a581f1218dd4bdc99be055cc102
Giovanni Basso <giovanni.basso@icloud.com>
2024-06-24 15:21:08 +0200

    debugging software

--- barra-monocolor/Software/dmx_in/dmx_in.ino
+++ barra-monocolor/Software/dmx_in/dmx_in.ino
@@ -1,49 +1,52 @@
-// D6 is DMX in
 
 #include <DMXSerial.h>
 
-const int RedPin = 5;
-const int GreenPin = 4;
-const int BluePin = 3;
+const int Laser1 = 3;
+const int Laser2 = 5;
+const int Laser3 = 6;
+const int Laser4 = 9;
 
 const int startChannel = 1;
 
-#define RedDefaultLevel 100
-#define GreenDefaultLevel 200
-#define BlueDefaultLevel 255
+#define L1DefaultLevel 255
+#define L2DefaultLevel 0
+#define L3DefaultLevel 0
+#define L4DefaultLevel 0
 
 
 void setup() {
+  // initialize dmx serial as receiver and set max channel to 4
   DMXSerial.init(DMXReceiver);
+  DMXSerial.maxChannel(4);
+  
+  DMXSerial.write(1, L1DefaultLevel);
+  DMXSerial.write(2, L2DefaultLevel);
+  DMXSerial.write(3, L3DefaultLevel);
+  DMXSerial.write(4, L4DefaultLevel);
 
-  DMXSerial.write(1, 120);
-  DMXSerial.write(2, 0);
-  DMXSerial.write(3, 0);
+  pinMode(Laser1, OUTPUT);
+  pinMode(Laser2, OUTPUT);
+  pinMode(Laser3, OUTPUT);
+  pinMode(Laser4, OUTPUT);
 
-  pinMode(RedPin, OUTPUT);
-  pinMode(GreenPin, OUTPUT);
-  pinMode(BluePin, OUTPUT);
+  analogWrite(Laser1, 255);
+  analogWrite(Laser2, 0);
+  analogWrite(Laser3, 0);
+  analogWrite(Laser4, 0);
 }
 
 void loop() {
   unsigned long lastPacket = DMXSerial.noDataSince();
 
-  if(lastPacket < 50000) {
-    if(DMXSerial.read(startChannel) > 100)
-      digitalWrite(RedPin, HIGH);
-    else
-      digitalWrite(RedPin, LOW);
-    if(DMXSerial.read(startChannel + 1) > 100)
-      digitalWrite(GreenPin, HIGH);
-    else
-      digitalWrite(GreenPin, LOW);
-    if(DMXSerial.read(startChannel + 2) > 100)
-      digitalWrite(BluePin, HIGH);
-    else
-      digitalWrite(BluePin, LOW);
+  if(lastPacket < 60000) {
+    analogWrite(Laser1, DMXSerial.read(startChannel));
+    analogWrite(Laser2, DMXSerial.read(startChannel + 1));
+    analogWrite(Laser3, DMXSerial.read(startChannel + 2));
+    analogWrite(Laser4, DMXSerial.read(startChannel + 3));
   } else {
-    digitalWrite(RedPin, HIGH);
-    digitalWrite(GreenPin, LOW);
-    digitalWrite(BluePin, LOW);
+    analogWrite(Laser1, L1DefaultLevel);
+    analogWrite(Laser2, L2DefaultLevel);
+    analogWrite(Laser3, L3DefaultLevel);
+    analogWrite(Laser4, L4DefaultLevel);
   }
 }
