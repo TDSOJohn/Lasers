74ab617d690a697ab6bf3cbbbb8a91f7f26b813d
ARtbibaOfficial <giovanni.basso@icloud.com>
2024-12-03 19:24:14 +0100

    improved arduino code

--- barra-monocolor/Software/dmx_in_DIP/dmx_in_DIP.ino
+++ barra-monocolor/Software/dmx_in_DIP/dmx_in_DIP.ino
@@ -1,105 +1,193 @@
-
 #include <DMXSerial.h>
 
 #include "data.h"
 
-int bar_id = 0;
+
+// ===============================================================================
+//    0    |    1    |    2    |    3    |    4    |    5    |    6    |    7    |
+//         .         .         .         .         .         .         .         .
+//    0:   .    0:   .         .         .         .         .         .         .
+// FUN MODE+-STATIC-----PTRN------PTRN------PTRN------PTRN------PTRN------PTRN   .
+//         |         .         .         .         .         .         .         .
+//         |    1:   .    0:   .         .         .         .         .         .
+//         +--ANIM---+---SEQ-----SPEED-----SPEED-----SPEED------PTRN------PTRN   .
+//         .         |         .         .         .         .         .         .
+//         .         |    1:   .         .         .         .         .         .
+//         .         +--RANDOM---SPEED-----SPEED-----SPEED------PTRN------PTRN   .
+//         .         .         .         .         .         .         .         .
+//    1:   .         .         .         .         .         .         .         .
+// DMX MODE--CH SEL----CH SEL----CH SEL----CH SEL----CH SEL----CH SEL----CH SEL  .
+//         .         .         .         .         .         .         .         .
+//         .         .         .         .         .         .         .         .
+// ===============================================================================
 
 const int Laser1 = 3;
 const int Laser2 = 5;
 const int Laser3 = 6;
 const int Laser4 = 9;
 
-int startChannel = 0;
 
-#define L1DefaultLevel 255
-#define L2DefaultLevel 255
-#define L3DefaultLevel 255
-#define L4DefaultLevel 255
+struct Settings {
+  //  0: static, 1: DMX, 2: anim
+  byte mode;
 
-#define DIP_CHANNEL_SIZE 6
-#define DIP_MODE_SIZE 2
+  //  0: sequential, 1: random
+  byte anim_type;
+  int speed;
+  byte pattern;
+  byte curr_pattern;
 
-byte speed = 20;
+  byte dmx_start_channel;
+};
 
-// 0 for ALL ON
-// 1 for FUN MODE
-// 2 for DMX MODE
-byte mode = 2;
+Settings settings;
 
 void setup() {
   // initialize dmx serial as receiver
   DMXSerial.init(DMXReceiver);
   
-  DMXSerial.write(1, L1DefaultLevel);
-  DMXSerial.write(2, L2DefaultLevel);
-  DMXSerial.write(3, L3DefaultLevel);
-  DMXSerial.write(4, L4DefaultLevel);
+  DMXSerial.write(1, 255);
+  DMXSerial.write(2, 255);
+  DMXSerial.write(3, 255);
+  DMXSerial.write(4, 255);
 
   pinMode(Laser1, OUTPUT);
   pinMode(Laser2, OUTPUT);
   pinMode(Laser3, OUTPUT);
   pinMode(Laser4, OUTPUT);
 
-  analogWrite(Laser1, L1DefaultLevel);
-  analogWrite(Laser2, L2DefaultLevel);
-  analogWrite(Laser3, L3DefaultLevel);
-  analogWrite(Laser4, L4DefaultLevel);
+  analogWrite(Laser1, 255);
+  analogWrite(Laser2, 255);
+  analogWrite(Laser3, 255);
+  analogWrite(Laser4, 255);
 
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  pinMode(A2, INPUT_PULLUP);
-  pinMode(A3, INPUT_PULLUP);
-  pinMode(A4, INPUT_PULLUP);
-  pinMode(A5, INPUT_PULLUP);
-  pinMode(A6, INPUT_PULLUP);
-  pinMode(A7, INPUT_PULLUP);
-  
-  delay(50);
-  bar_id = !digitalRead(A0);
-  delay(50);
-  bar_id += (!digitalRead(A1) * 2);
-  delay(50);
-  bar_id += (!digitalRead(A2) * 4);
-  delay(50);
-/*
-  int state_pin_0 = digitalRead(MODE_PINS[0]);
-  int state_pin_1 = digitalRead(MODE_PINS[1]);
+  for(int i = A0; i < A7; i++)
+    pinMode(i, INPUT_PULLUP);
 
-  if((state_pin_0 == HIGH) && (state_pin_1 == HIGH))
-    mode = 0;
-  else if((state_pin_0 == HIGH) && (state_pin_1 == LOW))
-    mode = 1;
-  else if((state_pin_0 == LOW) && (state_pin_1 == HIGH))
-    mode = 2;
-*/
-  startChannel = bar_id * 4 + 1;
+  settings.mode = !digitalRead(A0);
+  delay(30);
+  // fun mode
+  if(settings.mode == 0) {
+    // anim mode
+    if(!digitalRead(A1)) {
+      settings.mode = 2;
+      settings.anim_type = !digitalRead(A2);
+      settings.curr_pattern = 0;
+      settings.speed = !digitalRead(A3);
+      delay(30);
+      settings.speed += (!digitalRead(A4) * 2);
+      delay(30);
+      settings.speed += (!digitalRead(A5) * 4);
+      delay(30);
+      settings.pattern = !digitalRead(A6);
+      delay(30);
+      settings.pattern += (!digitalRead(A7) * 2);
+      delay(30);
+    }
+    // static mode
+    else {
+      settings.pattern = !digitalRead(A2);
+      delay(30);
+      settings.pattern += (!digitalRead(A3) * 2);
+      delay(30);
+      settings.pattern += (!digitalRead(A4) * 4);
+      delay(30);
+      settings.pattern += (!digitalRead(A5) * 8);
+      delay(30);
+      settings.pattern += (!digitalRead(A6) * 16);
+      delay(30);
+      settings.pattern += (!digitalRead(A7) * 32);
+      delay(30);
+    }
+  }
+  //  DMX mode
+  else {
+    settings.dmx_start_channel = !digitalRead(A1);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A2) * 2);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A3) * 4);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A4) * 8);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A5) * 16);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A6) * 32);
+    delay(30);
+    settings.dmx_start_channel += (!digitalRead(A7) * 64);
+    delay(30);
+  }
+
+  switch(settings.speed) {
+    case 0:
+      settings.speed = 1;
+      break;
+    case 1:
+      settings.speed = 2;
+      break;
+    case 2:
+      settings.speed = 5;
+      break;
+    case 3:
+      settings.speed = 10;
+      break;
+    case 4:
+      settings.speed = 25;
+      break;
+    case 5:
+      settings.speed = 50;
+      break;
+    case 6:
+      settings.speed = 100;
+      break;
+    case 7:
+      settings.speed = 250;
+      break;
+    case 8:
+      settings.speed = 500;
+      break;
+    default:
+      settings.speed = 1;
+      break;
+  }
 }
 
 int step = 0;
 
 void loop() {
   unsigned long lastPacket = DMXSerial.noDataSince();
+  //  DMX mode and receiving
+  if(settings.mode == 1 && lastPacket < 10000) {
+    analogWrite(Laser1, DMXSerial.read(settings.dmx_start_channel));
+    analogWrite(Laser2, DMXSerial.read(settings.dmx_start_channel + 1));
+    analogWrite(Laser3, DMXSerial.read(settings.dmx_start_channel + 2));
+    analogWrite(Laser4, DMXSerial.read(settings.dmx_start_channel + 3));
+  }
+  //  Fun mode
+  else if(settings.mode == 2) {
+    analogWrite(Laser1, data[settings.curr_pattern][step][0]);
+    analogWrite(Laser2, data[settings.curr_pattern][step][1]);
+    analogWrite(Laser3, data[settings.curr_pattern][step][2]);
+    analogWrite(Laser4, data[settings.curr_pattern][step][3]);
 
-  if(mode == 2 && lastPacket < 10000) {
-    analogWrite(Laser1, DMXSerial.read(startChannel));
-    analogWrite(Laser2, DMXSerial.read(startChannel + 1));
-    analogWrite(Laser3, DMXSerial.read(startChannel + 2));
-    analogWrite(Laser4, DMXSerial.read(startChannel + 3));
-  } else if(mode == 1) {
-    analogWrite(Laser1, data[step][0]);
-    analogWrite(Laser2, data[step][1]);
-    analogWrite(Laser3, data[step][2]);
-    analogWrite(Laser4, data[step][3]);
-    
-    step = rand() % 16;
-    
-    delay(1000 / speed);
-  } else {
-      analogWrite(Laser1, L1DefaultLevel);
-      analogWrite(Laser2, L2DefaultLevel);
-      analogWrite(Laser3, L3DefaultLevel);
-      analogWrite(Laser4, L4DefaultLevel);
+    //  Sequential type
+    if(settings.anim_type == 0) {
+      step = (step + 1) % 16;
+      if(step == 15)
+        settings.curr_pattern = (settings.curr_pattern + 1) % 4;
+    }
+    //  Random type
+    else {
+      step = rand() % 16;
+    }
+    delay(10000 / settings.speed);
+  }
+  //  Static mode
+  else {
+      analogWrite(Laser1, 255);
+      analogWrite(Laser2, 255);
+      analogWrite(Laser3, 255);
+      analogWrite(Laser4, 255);
       delay(100);
   }
 }
